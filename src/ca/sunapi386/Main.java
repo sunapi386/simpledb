d55438a5a2ee8a020ba2b723e75b87dbe9f38626
Jason Sun <sunapi386@gmail.com>
2014-08-23 11:46:24 -0700

    refactor to storage class

--- src/ca/sunapi386/Main.java
+++ src/ca/sunapi386/Main.java
@@ -1,62 +1,51 @@
 package ca.sunapi386;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 import static java.lang.System.in;
-import static java.lang.System.out;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanInput = new Scanner(in);
         String[] tokens;
-        Map<String, String> keyValueMap = new HashMap<String, String>();
-        Map<String, Integer> numEqualToMap = new HashMap<String, Integer>();
+        Storage storage = new Storage();
+
 
         for (; ; ) {
             tokens = scanInput.nextLine().split(" ");
             String cmd = tokens[0];
-
             if (cmd.equals("SET")) {
+                if (tokens.length != 3) {
+                    continue;
+                }
                 String name = tokens[1];
                 String value = tokens[2];
-                String previousValue = keyValueMap.containsKey(name) ? keyValueMap.get(name) : null;
-                keyValueMap.put(name, value);
-                if (numEqualToMap.containsKey(previousValue)) {
-                    int updatedCount = numEqualToMap.get(previousValue) - 1;
-                    numEqualToMap.put(previousValue, updatedCount);
-                }
-                if (numEqualToMap.containsKey(value)) {
-                    Integer currentCount = numEqualToMap.get(value);
-                    numEqualToMap.put(value, currentCount + 1);
-                } else {
-                    numEqualToMap.put(value, 1);
-                }
-
-
+                storage.set(name, value);
+                Storage.set(name, value);
             } else if (cmd.equals("GET")) {
-                String name = tokens[1];
-                if (keyValueMap.containsKey(name)) {
-                    System.out.println(keyValueMap.get(name));
-                } else {
-                    System.out.println("NULL");
+                if (tokens.length != 2) {
+                    continue;
                 }
-            } else if (cmd.equals("UNSET")) {
                 String name = tokens[1];
-                String value = keyValueMap.get(name);
-                keyValueMap.remove(name); // does this break if its not in there
-                int newCount = numEqualToMap.get(value) - 1;
-                numEqualToMap.put(value, newCount);
+                Storage.get(name);
+            } else if (cmd.equals("UNSET")) {
+                if (tokens.length != 2) {
+                    continue;
+                }
+                String name = tokens[1];
+                storage.unset(name);
             } else if (cmd.equals("NUMEQUALTO")) {
+                if (tokens.length != 2) {
+                    continue;
+                }
                 String value = tokens[1];
-                Integer count = numEqualToMap.get(value);
-                System.out.println(count == null ? 0 : count);
+                Storage.numEqualTo(value);
             } else if (cmd.equals("END")) {
                 break;
             }
         }
 
     }
+
 }
---
+++ src/ca/sunapi386/Storage.java
@@ -0,0 +1,44 @@
+package ca.sunapi386;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Storage {
+    private static Map<String, String> keyValueMap = new HashMap<String, String>();
+    private static Map<String, Integer> numEqualToMap = new HashMap<String, Integer>();
+
+    public static void set(String name, String value) {
+        String previousValue = keyValueMap.containsKey(name) ? keyValueMap.get(name) : null;
+        keyValueMap.put(name, value);
+        if (numEqualToMap.containsKey(previousValue)) {
+            int updatedCount = numEqualToMap.get(previousValue) - 1;
+            numEqualToMap.put(previousValue, updatedCount);
+        }
+        if (numEqualToMap.containsKey(value)) {
+            Integer currentCount = numEqualToMap.get(value);
+            numEqualToMap.put(value, currentCount + 1);
+        } else {
+            numEqualToMap.put(value, 1);
+        }
+    }
+
+    public static void numEqualTo(String value) {
+        Integer count = numEqualToMap.get(value);
+        System.out.println(count == null ? 0 : count);
+    }
+
+    public void unset(String name) {
+        String value = keyValueMap.get(name);
+        keyValueMap.remove(name); // does this break if its not in there
+        int newCount = numEqualToMap.get(value) - 1;
+        numEqualToMap.put(value, newCount);
+    }
+
+    public static void get(String name) {
+        if (keyValueMap.containsKey(name)) {
+            System.out.println(keyValueMap.get(name));
+        } else {
+            System.out.println("NULL");
+        }
+    }
+}
